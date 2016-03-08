9fcb4b75d79e546b34a7892837f13220e2bcf555
franchescamansour <cocomansour@Franchesca.local>
2016-03-08 16:38:08 -0500

    zdfvS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
 var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
+var $h2 = $('h2');
+var $img = $('img');
+var $dl = $('dl');
+var $dt = $('dt');
 
 $header.addClass('masthead');
 
-$('.big-heading').remove();
-$h1.addClass('.logo');
+$h1.removeClass('big-heading');
+$h1.addClass('logo');
 
 $main.addClass('wrapper');
 
+$('main p:first-child').addClass('intro');
 
+$h2.addClass('slug-head');
+
+$('ul').addClass('slug-list');
+
+$img.addClass('slug-img');
+
+$('ul li:first-child').addClass('slug-list-first');
+$('ul li:last-child').addClass('slug-list-last');
+
+$dl.addClass('.classification');
+
+$dt.removeClass('big-label');
+
+$('ul li:last-child').show();
