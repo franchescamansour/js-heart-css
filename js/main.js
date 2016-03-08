f9cc2ae906c35e4d612bc0477a634234c6ac6735
franchescamansour <cocomansour@Franchesca.local>
2016-03-08 16:01:42 -0500

    Adding classes and removing classes

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+
+$header.addClass('masthead');
+
+$('.big-heading').remove();
+$h1.addClass('.logo');
+
+$main.addClass('wrapper');
+
+
